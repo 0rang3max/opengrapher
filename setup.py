a0ac980a6f457ecc31ca534829998e83dc5b5cfb
m.semenov <m.semenov@corp.mail.ru>
2020-06-04 20:48:28 +0300

    fixed build

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import setup
 
-version = "0.1"
+
+with open("README.md") as f:
+    long_description = f.read()
+
 
 setup(
     name="opengrapher",
-    version=version,
-    description="A module to parse the OpenGraph Protocol",
-    long_description=open("README.rst").read() + "\n",
+    packages=["opengrapher"],
+    version="0.1",
+    description="Utility for parsing the OpenGraph tags",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Programming Language :: Python",
         "Topic :: Text Processing :: Markup :: HTML",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
     ],
-    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords="opengraph",
     author="Maxim Semenov",
     author_email="0rang3max@gmail.com",
     url="https://github.com/0rang3max/opengrapher",
+    download_url="https://github.com/0rang3max/opengrapher/archive/v_01.tar.gz",
     license="MIT",
-    packages=find_packages(exclude=["ez_setup", "tests"]),
     include_package_data=True,
-    zip_safe=False,
     python_requires=">=3.4",
     install_requires=["beautifulsoup4", "requests"],
     entry_points="""
